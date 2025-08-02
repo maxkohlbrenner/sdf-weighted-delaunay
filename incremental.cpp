48c4b481a2546555aeac501448a47d0f51a6b1d1
Max Kohlbrenner <kohlbrenner@tu-berlin.de>
2025-08-02 16:18:49 +0200

    Solve merge problems, revert back to a single-file structure

--- incremental.cpp
+++ incremental.cpp
@@ -7,10 +7,58 @@
 #include "polyscope/surface_mesh.h"
 #include "polyscope/volume_mesh.h"
 
-// #include "src/IRT.hpp"
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Regular_triangulation_3.h>
+#include <CGAL/Regular_triangulation_cell_base_3.h>
+#include <CGAL/Regular_triangulation_vertex_base_3.h>
+#include <CGAL/Triangulation_vertex_base_with_info_3.h>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel         K;
+typedef K::FT                                                       Weight;
+typedef K::Point_3                                                  Point;
+typedef K::Weighted_point_3                                         Weighted_point;
+typedef CGAL::Regular_triangulation_vertex_base_3<K>                Vb0;
+typedef CGAL::Triangulation_vertex_base_with_info_3<int, K, Vb0>    Vb;
+typedef CGAL::Regular_triangulation_cell_base_3<K>                  Cb;
+typedef CGAL::Triangulation_data_structure_3<Vb,Cb>                 Tds;
+typedef CGAL::Regular_triangulation_3<K, Tds>                       Rt;
+typedef Tds::Vertex_handle                                          Vertex_handle;
+typedef Tds::Cell_handle                                            Cell_handle;
 
 #include <filesystem>
 namespace fs = std::filesystem;
+
+// ----------------------- REFINEMENT ------------------------------
+class Candidate {
+    public:
+    Vertex_handle m_v0;
+    Vertex_handle m_v1;
+    Vertex_handle m_v2;
+    Vertex_handle m_v3;
+    double m_score;
+    Point m_point;
+
+	Candidate(  Vertex_handle v0,
+                Vertex_handle v1,
+                Vertex_handle v2,
+                Vertex_handle v3,
+				Point p,
+                double score ) : 
+                m_v0{v0},m_v1{v1},m_v2{v2},m_v3{v3},m_point{p},m_score{score}
+    {}
+  // ~CCandidate() {} 
+};
+
+struct less_refinement
+{ 
+  bool operator()(const Candidate& c1, 
+                  const Candidate& c2) const
+  {
+    return (c1.m_score < c2.m_score);
+  }
+};
+typedef typename std::priority_queue<Candidate, std::vector<Candidate>,less_refinement > PQueue;
+
+// ------------------------------------------------------------------
 
 void sample_sdf(int res, Eigen::MatrixXd &G, std::function<double(double,double,double)> sdf){
     G.resize(res*res*res, 4);
@@ -428,7 +476,8 @@ int main(int argc, char *argv[])
         std::string method_string((is_delaunay)? "DTMTR":"RegMTR");
         */
 
-        if (true) {
+        /*
+        if (false) {
             polyscope::init();
             polyscope::registerSurfaceMesh("Ground Truth", V, F);
             auto s = polyscope::registerPointCloud("Samples", S.block(0,0,S.rows(),3));
@@ -454,7 +503,7 @@ int main(int argc, char *argv[])
             polyscope::options::groundPlaneMode = polyscope::GroundPlaneMode::None;
             polyscope::show();
         } 
-        /*
+
         if (write) {
             fmt::print("Write files to folder {}\n", outpath);
             std::string filename = std::string(fs::path(argv[1]).filename());
